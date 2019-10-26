0b6ce476ca432515fe3457fc4e56a22335ebbea8
NQuinzon <51680780+NQuinzon@users.noreply.github.com>
2019-10-26 10:29:02 -0700

    Add files via upload

--- Basic_JavaScript_6.js
+++ Basic_JavaScript_6.js
@@ -13,14 +13,14 @@ function Vehicle(Make, Model, Year, Color) {
 }
 var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
 var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
-var Eric = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
 function myFunction() {
     document.getElementById("Keywords_and_Constructors").innerHTML =
     "Erik drives a " + Erik.Vehicle_Color + "-colored" + Erik.Vehicle_Model + " manufactured in " + Erik.Vehicle_Year;
 }
 
 function count_Function() {
-    document.getElementById("Nested_Function".innerHTML = Count();
+    document.getElementById("Nested_Function").innerHTML = Count();
     function Count() {
         var Starting_point = 9;
         function Plus_one() {Starting_point += 1;}
